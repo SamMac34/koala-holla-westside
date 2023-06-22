6e9c904528f44b504b06694bebf48b8e5e1f5221
Amy Smith <smithniamyfse@gmail.com>
2023-06-22 14:22:41 -0500

    updated delete request on both koal a.router.js and client.js

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -14,5 +14,22 @@ const koalaRouter = express.Router();
 
 
 // DELETE
+router.delete('/:id', (req, res) => {
+    let idToDelete = req.params.id;
+    // Use query parameterization to protect the 
+    // database from SQL injection
+    let query = `DELETE FROM "koalas" WHERE id = $1`;
+    // Connect/talk with the database and run the query
+    pool.query(query, [idToDelete])
+        .then((results) => {
+            console.log('koala deleted');
+            // Send status 200 or 'ok' to the client
+            res.sendStatus(200);
+        }).catch((error) => {
+            console.log('Error making database query - in router.delete: ', error);
+            // Send status code 500 or 'internal server error'
+            res.sendStatus(500);
+        });
+})
 
 module.exports = koalaRouter;
