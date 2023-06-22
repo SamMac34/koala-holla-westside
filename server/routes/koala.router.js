a8ca14eaa943fd1189170751398aeafea39dbfa8
ZakariyeAden <zakariye5417@gmail.com>
2023-06-22 15:06:04 -0500

    updating POST

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,21 +1,33 @@
-const express = require('express');
+const express = require("express");
 const koalaRouter = express.Router();
-const koalas = require('../public/Modules/koalas')
+
 
 // DB CONNECTION
 
-
 // GET
-
 
 // POST
 // Post the Koalas
-koalaRouter.post('/', (req,res) => {
-  // Push and POST the Data that the user submits.
-  koalas.push(req.body);
-  res.sendStatus(201);
-})
+koalaRouter.post("/", (req, res) => {
+  let newKoala = req.body;
+  // Insert and use pararmeteraiztion for POST
+  let queryText = `INSERT INTO "Koalas" ("name", "gender", "age", "ready_to_transfer","notes")
+                   VALUES ($1, $2,$3,$4);`;
 
+  let koalasParams = [newKoala.name, newKoala.gender, newKoala.age,newKoala.ready_to_transfer];
+  // Use the Query and Pool
+   pool.query(queryText, koalasParams)
+    // Get the Response
+   .then((response) => {
+    //  Send an Ok - Status
+     res.sendStatus(201);
+    //  Catch any Errors
+   }).catch((error) => {
+     console.log(`Catch any Errors ${queryText}`, error);
+    //  Send me an Error
+     res.sendStatus(500);
+   })
+});
 
 // PUT
 
