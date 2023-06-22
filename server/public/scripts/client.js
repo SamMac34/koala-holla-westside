350e5cbf4727d9113b09ca219093e5949c8552fb
Sam McCauley <sammccauley34@gmail.com>
2023-06-22 15:42:42 -0500

    merging main with feature

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -6,10 +6,13 @@ $( document ).ready( function(){
   setupClickListeners()
   // load existing koalas on page load
   getKoalas();
+  
 
 }); // end doc ready
 
 function setupClickListeners() {
+  // Listener to update koala transfer status
+  $( '#viewKoalas' ).on( 'click', '.transfer-btn', transferReady )
   $( '#addButton' ).on( 'click', function(){
     console.log( 'in addButton on click' );
     // get user input and put in an object
@@ -27,14 +30,43 @@ function setupClickListeners() {
   }); 
 }
 
+// initial GET request for ALL koalas
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
-  
+  $.ajax({
+    type: 'GET',
+    url: '/koalas'
+  }).then((response) => {
+    // create console log to make sure it works
+    console.log('GET /koalas response:', response);
+    // render koalas
+    // render(response);
+  })
 } // end getKoalas
+
+
 
 function saveKoala( newKoala ){
   console.log( 'in saveKoala', newKoala );
   // ajax call to server to get koalas
  
+}
+
+// TODO ADD PUT req to update transfer status
+function transferReady() {
+  console.log('in transferReady');
+  const koalaId = $(this).parent().parent().data('id');
+
+  $.ajax({
+    method: 'PUT',
+    url: `/koalas/${koalaId}`
+  }).then((response) => {
+    console.log( 'Koala ready for transfer!' );
+    getKoalas();
+  }).catch((error) => {
+    console.log( 'Error changing transfer status', error )
+    alert( 'Transfer status NOT updated!' );
+    resizeBy.sendstatus(500);
+  });
 }
