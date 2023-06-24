7e205a20ea4d6cb481aec446f082d512eeeb1d13
Amy Smith <smithniamyfse@gmail.com>
2023-06-23 21:16:26 -0500

    updated index.html naming, added commentary to koala.router.js and organized structure better

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -1,45 +1,65 @@
+// Database (Koala_Holla) Connection
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 
 // GET all koalas from the database
 router.get('/', (req, res) => {
+  // write SQL query and save that in a variable
   const queryText = 'SELECT * FROM "Koalas";';
-
-  pool
-    .query(queryText)
+  // send SQL query to the database using pool.query
+  pool.query(queryText)
+    // best practice is to use the word 'result' to describe
+    // what we get back from the Koala_Holla database
     .then((result) => {
       console.log('result is:', result);
+      // result.rows is where the data we requested is
       res.send(result.rows);
     })
     .catch((error) => {
       console.log('Error making query:', error);
       res.sendStatus(500);
     });
-});
+}); // end GET all koalas from the database
+
+
+
 
 // GET specific koala by ID
+  // id is a route parameter
+    // we use this parameter to identify
+    // that we want this specific identified part of the request
 router.get('/:id', (req, res) => {
+  // assign req.params.id to a variable
+  // write a SQL query that gets back the koala with the
+    // specified (parameter) id
   const idToGet = req.params.id;
+
   const query = 'SELECT * FROM "Koalas" WHERE id = $1;';
-  
-  pool
-    .query(query, [idToGet])
+  // use pool.query to access pool 
+    // (group of connections between server and database)
+  pool.query(query, [idToGet])
+    // .then because query is asynchronous
     .then((result) => {
       console.log(`Koala with id of ${idToGet}:`, result.rows);
+      // result.rows is where the desired part of the
+        // result/response is
+        // send data back to client
       res.send(result.rows);
     })
     .catch((error) => {
       console.log('Error making database query:', error);
       res.sendStatus(500);
     });
-});
+}); // end GET specific koala by ID
+
+
 
 // POST a new koala
 router.post('/', (req, res) => {
   const newKoala = req.body;
   console.log('Adding koala', newKoala);
-
+  // Insert and use parameterization for POST
   const queryText = `INSERT INTO "Koalas" ("name", "age", "gender", "ready_to_transfer", "notes")
                      VALUES ($1, $2, $3, $4, $5);`;
 
@@ -51,18 +71,23 @@ router.post('/', (req, res) => {
     newKoala.notes,
   ];
 
-  pool
-    .query(queryText, koalaParams)
+  // Use the Query and Pool
+  pool.query(queryText, koalaParams)
+    // Get the result of query
     .then((result) => {
+      //  Send an Ok - Status
       res.sendStatus(201);
     })
+    // Catch any errors that occur
     .catch((error) => {
       console.log('Error making database query:', error);
       res.sendStatus(500);
     });
-});
+}); // end POST a new koala
 
-// UPDATE koala ready_to_transfer value to 'Y' for a specific ID
+
+
+// PUT koala ready_to_transfer value to 'Y' for a specific ID
 router.put('/:id', (req, res) => {
   const idToUpdate = req.params.id;
   const query = `UPDATE "Koalas" SET "ready_to_transfer" = 'Y' WHERE "id" = $1;`;
@@ -82,147 +107,23 @@ router.put('/:id', (req, res) => {
 // DELETE a koala by ID
 router.delete('/:id', (req, res) => {
   const idToDelete = req.params.id;
+  // Use query parameterization to protect the 
+    // database from SQL injection
   const query = `DELETE FROM "Koalas" WHERE "id" = $1`;
 
-  pool
-    .query(query, [idToDelete])
+  // Connect/talk with the database and run the query
+  pool.query(query, [idToDelete])
     .then((result) => {
       console.log('Koala deleted');
+      // Send status 200 or 'ok' to the client
       res.sendStatus(200);
     })
     .catch((error) => {
       console.log('Error making database query:', error);
+      // Send status code 500 or 'internal server error'
       res.sendStatus(500);
     });
 });
 
 module.exports = router;
 
-
-// const express = require('express');
-// const pool = require('../modules/pool');
-// const router = express.Router();
-
-
-// // DB CONNECTION
-
-// // GET
-// // GET all koalas from database
-// router.get('/', (req, res) => {
-//     // write SQL query and save that in a variable
-//     let queryText = 'SELECT * FROM "Koalas";';
-
-//     // send SQL query to the database using pool.query
-//     pool.query(queryText)
-//     // convention is to use the word 'result to describe
-//     // what we get back from the database
-//         .then((result) => {
-//             console.log('result is: ', result);
-//             //result.rows is where the data we requested is
-//             res.send(result.rows);
-//         }).catch ((error) => {
-//             console.log('Error making query:', error)
-//             res.sendStatus(500);
-//         })
-// });
-
-
-
-// // GET for specific id
-// router.get('/:id', (req, res) => {
-//     // id is a route parameter
-//     // we use this parameter to identify
-//     // that we want this specific identified part of the request
-//     console.log(req.params.id);
-//     // assign req.params.id to a variable
-//     const idToGet = req.params.id;
-
-//     // write a SQL query that gets back the koala with the
-//     // specified (parameter) id
-//     const query = `SELECT * FROM "Koalas" WHERE id=$1;`;
-
-//     // use pool.query to access pool 
-//     // (group of connections between server and database)
-//     pool.query(query, [idToGet])
-
-//     // .then because query is asynchronous
-//         .then((result) => {
-            
-//             // result.rows is where the desired part of the
-//             // result/response is
-//             console.log(`Koala with id of ${idToGet}`, result.rows);
-
-//             // send data back to client
-//             res.sendStatus(200);
-//         }).catch((error) => {
-//             console.log('error making database query: ', error);
-//             res.sendStatus(500);
-//         })
-
-// })
-
-// // POST
-// // Post the Koalas
-// router.post("/", (req, res) => {
-//   let newKoala = req.body;
-//   console.log('Adding koala', newKoala)
-//   // Insert and use parameterization for POST
-//   let queryText = `INSERT INTO "Koalas" ("name", "age", "gender", "ready_to_transfer", "notes")
-//                    VALUES ($1, $2, $3, $4, $5);`;
-// // let queryText = `INSERT INTO "Koalas" ("name", "gender", "age", "ready_to_transfer", "notes")
-// // VALUES ($1, $2, $3, $4, $5);`;
-
-//     let koalasParams = [newKoala.name, newKoala.age, newKoala.gender, newKoala.ready_to_transfer, newKoala.notes];
-//     // let koalasParams = [newKoala.name, newKoala.gender, newKoala.age, newKoala.ready_to_transfer];
-//   // Use the Query and Pool
-//    pool.query(queryText, koalasParams)
-//     // Get the Response
-//    .then(result => {
-//     //  Send an Ok - Status
-//      res.sendStatus(201);
-//     //  Catch any Errors
-//    })
-//    .catch(error => {
-//      console.log(`Catch any Errors ${queryText}`, error);
-//     //  Send me an Error
-//      res.sendStatus(500);
-//    });
-// });
-
-
-
-// // PUT request to update transfer value YES/NO 
-// router.put('/:id', (req, res) => {
-//     let idToUpdate = req.params.id;
-//     let query = `UPDATE "Koalas" SET "ready_to_transfer" = 'Y' WHERE "id" = $1;`;
-//     pool.query(query, [idToUpdate])
-//     .then((results) => {
-//         console.log('Koala is Ready to transfer!', results);
-//         res.sendStatus(200)
-//     }).catch((error) => {
-//         console.log('Error changing Koala transfer status', error)
-//         res.sendStatus(500)
-//     });
-// });
-
-
-// // DELETE
-// router.delete('/:id', (req, res) => {
-//     let idToDelete = req.params.id;
-//     // Use query parameterization to protect the 
-//     // database from SQL injection
-//     let query = `DELETE FROM "Koalas" WHERE "id" = $1`;
-//     // Connect/talk with the database and run the query
-//     pool.query(query, [idToDelete])
-//         .then((results) => {
-//             console.log('koala deleted');
-//             // Send status 200 or 'ok' to the client
-//             res.sendStatus(200);
-//         }).catch((error) => {
-//             console.log('Error making database query - in router.delete: ', error);
-//             // Send status code 500 or 'internal server error'
-//             res.sendStatus(500);
-//         });
-// })
-
-// module.exports = router;
